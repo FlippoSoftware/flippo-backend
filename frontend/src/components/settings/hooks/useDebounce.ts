c21e1a5b70ce39d59599565fa0e681f22bcd2ff7
BlackPoretsky <84518407+BlackPoretsky@users.noreply.github.com>
2024-09-19 14:06:10 +0300

    Feat: debounce, click outside hooks have been created

---
+++ frontend/src/components/settings/hooks/useDebounce.ts
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+
+const useDebounce = <T>(value: T, delay: number = 200): T => {
+  const [debounceValue, setDebounceValue] = useState<T>(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setDebounceValue(value), delay);
+    return () => clearTimeout(timer);
+  }, [value, delay]);
+
+  return debounceValue;
+};
+
+export { useDebounce };
