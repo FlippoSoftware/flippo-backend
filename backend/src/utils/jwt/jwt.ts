b7656abd83ebd653fbc8a0a9f6e5e851169d7abc
BlackPoretsky <84518407+BlackPoretsky@users.noreply.github.com>
2024-09-23 20:34:36 +0300

    Updated: token generation has been updated

--- backend/src/utils/jwt/jwt.ts
+++ backend/src/utils/jwt/jwt.ts
@@ -1,25 +1,18 @@
 import * as jose from "jose";
-import { TConnectionData, TRefreshJwtID, TRefreshToken } from "@schemas/index.ts";
+import { TRefreshJwtID } from "@schemas/index.ts";
 import { getExpiration, getIssuedAt } from "./utils/timestamps.ts";
 import generateUUID from "./utils/uuid.ts";
-import isTimeExpired from "./utils/isTimeExpired.ts";
-import instanceOfA from "./utils/instanceOfA.ts";
 import {
   AccessPayloadSchema,
   RefreshPayloadSchema,
   TAccessPayload,
-  TRefreshPayload,
-  TUserID,
-  UserIDSchema
+  TRefreshPayload
 } from "./types/IAuthPayload.ts";
 import { TTokens } from "./types/TTokens.ts";
-import {
-  createRefreshToken,
-  revokeRefreshToken,
-  fetchRefreshToken,
-  deleteRefreshToken
-} from "@dao/refreshToken.dao.ts";
 import { SurrealDBEnv, AuthEnv } from "@schemas/index.ts";
+import { TUserID, UserIDSchema } from "@schemas/db/user.schema.ts";
+import { PrivateKeyObject } from "./utils/keys/privateKey.ts";
+import { PublicKeyObject } from "./utils/keys/publicKey.ts";
 
 function generateAccessTokenPayload(userID: TUserID) {
   const iat: number = getIssuedAt();
@@ -33,9 +26,9 @@ function generateAccessTokenPayload(userID: TUserID) {
     AC: SurrealDBEnv.SURREALDB_AC,
     iat: iat,
     exp: expAccess
-  } satisfies TAccessPayload;
+  } as TAccessPayload;
 
-  return payload;
+  return AccessPayloadSchema.parse(payload);
 }
 
 function generateRefreshTokenPayload(userID: TUserID) {
@@ -49,12 +42,15 @@ function generateRefreshTokenPayload(userID: TUserID) {
     userID: userID,
     iat: iat,
     exp: expRefresh
-  } satisfies TRefreshPayload;
+  } as TRefreshPayload;
 
-  return payload;
+  return RefreshPayloadSchema.parse(payload);
 }
 
-async function generateTokens(userID: TUserID, connectionData: TConnectionData): Promise<TTokens> {
+async function generateTokens(userID: TUserID): Promise<{
+  tokens: TTokens;
+  payloads: { accessPayload: TAccessPayload; refreshPayload: TRefreshPayload };
+}> {
   const parseRes = UserIDSchema.safeParse(userID);
   if (!parseRes.success) {
     throw new TypeError("Invalid user ID");
@@ -68,74 +64,28 @@ async function generateTokens(userID: TUserID, connectionData: TConnectionData):
   const accessToken: string = await generateToken(accessPayload);
   const refreshToken: string = await generateToken(refreshPayload);
 
-  const refreshTokenDB: TRefreshToken | undefined = await createRefreshToken({
-    id: refreshPayload.jti,
-    user: userID,
-    isRevoked: false,
-    connectionData
-  } as TRefreshToken);
-
-  if (!refreshTokenDB) {
-    throw new Error("Failed to create a refresh token");
-  }
-
-  return { accessToken, refreshToken };
-}
-
-async function refreshTokens(
-  refreshToken: string,
-  connectionData: TConnectionData
-): Promise<TTokens> {
-  const { userID, jti, exp }: TRefreshPayload = (await verifyToken(
-    refreshToken
-  )) as TRefreshPayload;
-
-  if (isTimeExpired(exp)) {
-    await deleteRefreshToken(jti);
-    throw new Error("Refresh token has expired!");
-  }
-
-  // TODO: implement a token selection to check if it has been revoked
-  const tokenDB: TRefreshToken | undefined = await fetchRefreshToken(jti);
-  if (!tokenDB) {
-    throw new Error("There is no such refresh token!");
-  }
-
-  if (tokenDB.isRevoked) {
-    throw new Error("The refresh token has been revoked!");
-  }
-
-  const tokens: Promise<TTokens> = generateTokens(userID, connectionData);
-
-  // TODO: implement token invalidation
-  await revokeRefreshToken(jti);
-
-  return tokens;
+  return { tokens: { accessToken, refreshToken }, payloads: { accessPayload, refreshPayload } };
 }
 
 async function generateToken(payload: TAccessPayload | TRefreshPayload): Promise<string> {
-  payload = instanceOfA<TAccessPayload>(payload)
-    ? AccessPayloadSchema.parse(payload)
-    : RefreshPayloadSchema.parse(payload);
-
-  const privateKey: Uint8Array = new TextEncoder().encode(AuthEnv.AUTH_PRIVATE_KEY);
-
   const token: string = await new jose.SignJWT(payload)
     .setProtectedHeader({ alg: AuthEnv.AUTH_HASH_ALG })
-    .sign(privateKey);
+    .sign(PrivateKeyObject);
 
   return token;
 }
 
-async function verifyToken(token: string): Promise<TRefreshPayload | TAccessPayload | undefined> {
-  const publicKey: Uint8Array = new TextEncoder().encode(AuthEnv.AUTH_PUBLIC_KEY);
-  const options: jose.JWTVerifyOptions = { issuer: AuthEnv.AUTH_ISS };
+async function verifyToken(token: string): Promise<TRefreshPayload | TAccessPayload> {
+  const options: jose.JWTVerifyOptions = {
+    algorithms: [AuthEnv.AUTH_HASH_ALG],
+    issuer: AuthEnv.AUTH_ISS
+  };
 
   const payload: TRefreshPayload | TAccessPayload = (
-    await jose.jwtVerify(token, publicKey, options)
+    await jose.jwtVerify(token, PublicKeyObject, options)
   ).payload as TRefreshPayload | TAccessPayload;
 
   return payload;
 }
 
-export { generateTokens, refreshTokens, verifyToken };
+export { generateTokens, verifyToken };
