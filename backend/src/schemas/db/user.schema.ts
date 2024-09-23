736d97c459c1057614c359fcd3abe4c9e387c688
BlackPoretsky <84518407+BlackPoretsky@users.noreply.github.com>
2024-09-23 17:51:05 +0300

    Update: added scheme for creating a user

--- backend/src/schemas/db/user.schema.ts
+++ backend/src/schemas/db/user.schema.ts
@@ -1,23 +1,55 @@
 import { z } from "zod";
 import { record } from "./record.schema.ts";
+import { providerId } from "@schemas/utils/providerId.schema.ts";
 
-const UserRole = z.union([z.literal("admin"), z.literal("user"), z.literal("premium")]);
-type TUserRole = z.infer<typeof UserRole>;
+const UserRoleSchema = z.union([z.literal("admin"), z.literal("user"), z.literal("premium")]);
+type TUserRole = z.infer<typeof UserRoleSchema>;
+
+const UserIDSchema = record("user");
+type TUserID = z.infer<typeof UserIDSchema>;
+
+const ProviderIDSchema = z.union([
+  providerId("google"),
+  providerId("vkontakte"),
+  providerId("email")
+]);
+type TProviderID = z.infer<typeof ProviderIDSchema>;
 
 const UserSchema = z.object({
-  id: record("user"),
+  id: UserIDSchema,
   name: z.string(),
   surname: z.string(),
   patronymic: z.string().optional(),
   username: z.string().optional(),
-  email: z.string(),
-  password: z.string(),
-  avatarURL: z.string().url().optional(),
-  role: UserRole,
+  email: z.string().email(),
+  providersID: z.array(ProviderIDSchema),
+  image: z.string().url().optional(),
+  role: UserRoleSchema,
   created: z.coerce.date(),
   updated: z.coerce.date()
 });
 
 type TUser = z.infer<typeof UserSchema>;
 
-export { UserRole, type TUserRole, UserSchema, type TUser };
+const CreateUserSchema = z.object({
+  body: UserSchema.omit({
+    id: true,
+    created: true,
+    updated: true
+  })
+});
+
+type TCreateUser = z.infer<typeof CreateUserSchema>;
+
+export {
+  UserRoleSchema,
+  type TUserRole,
+  UserIDSchema,
+  type TUserID,
+  ProviderIDSchema,
+  type TProviderID,
+  UserSchema,
+  type TUser,
+  CreateUserSchema,
+  type TCreateUser
+};
