4fd49fa629fbcaf735dc6f5c62a316ec9d182ec5
BlackPoretsky <84518407+BlackPoretsky@users.noreply.github.com>
2024-10-19 19:12:40 +0300

    Feat: controller for user

--- backend/src/controller/user.controller.ts
+++ backend/src/controller/user.controller.ts
@@ -1,103 +1,225 @@
-import { TCreateUser } from "@schemas/db/user.schema.ts";
-import { TConnectionData } from "@schemas/index.ts";
-import { TokenService } from "@service/refreshToken.service.ts";
+import { URLSearchParams } from "url";
+
+import { type Request, type Response, type NextFunction } from "express";
+import { ENV, type TCreateUser, type TUser } from "@schemas/index.ts";
+import { OAuthService } from "@service/oauth.service.ts";
+import { type IGoogleTokensResult } from "@service/types/IGoogleTokensResult.ts";
+import { type IGoogleUserResult } from "@service/types/IGoogleUserResult.ts";
+import { type IVkontakteTokensResult } from "@service/types/IVkontakteTokensResult.ts";
+import { type IVkontakteUserResult } from "@service/types/IVkontakteUserResult.ts";
+import { type IYandexTokensResult } from "@service/types/IYandexTokensResult.ts";
+import { type IYandexUserResult } from "@service/types/IYandexUserResult.ts";
 import { UserService } from "@service/user.service.ts";
-import stringToUnixSeconds from "@utils/jwt/utils/stringToUnixSeconds.ts";
-import logger from "@utils/logger.ts";
-import { CookieOptions, Request, Response, NextFunction } from "express";
+import getConnectionData from "@utils/getConnectionData.ts";
+import omitUndefined from "@utils/omitUndefined.ts";
 import { ApiError } from "src/exceptions/api.error.ts";
+import { type TTokens } from "@utils/jwt/types/TTokens.ts";
+import { EmailService } from "@service/email.service.ts";
 
-const accessTokenCookieOptions: CookieOptions = {
-  maxAge: stringToUnixSeconds("15min"), // 15 mins
-  httpOnly: true,
-  domain: "localhost",
-  path: "/",
-  sameSite: "lax",
-  secure: false
-};
+import {
+  accessTokenClearCookieOptions,
+  accessTokenCookieOptions,
+  refreshTokenClearCookieOptions,
+  refreshTokenCookieOptions,
+  registrationEmailClearCookieOptions
+} from "./constant/cookieOption.ts";
 
-const refreshTokenCookieOptions: CookieOptions = {
-  ...accessTokenCookieOptions,
-  maxAge: stringToUnixSeconds("24w") // 1 year
-};
+function composeAnAuthResponse(res: Response, tokens: TTokens, user: TUser) {
+  res.cookie("refreshToken", tokens.refreshToken, refreshTokenCookieOptions);
+  res.cookie("accessToken", tokens.accessToken, accessTokenCookieOptions);
+
+  const composeData = {
+    userId: user.id,
+    role: user.role,
+    email: user.email,
+    image: user.image,
+    name: user.name,
+    surname: user.surname,
+    username: user.username
+  };
+
+  return composeData;
+}
+
+async function authProcess(req: Request, res: Response, authUser: TCreateUser) {
+  const connectionData = getConnectionData(req);
+
+  let userData = await UserService.signIn(authUser.providersId[0], connectionData);
+  if (!userData.user) userData = await UserService.signUp(authUser, connectionData);
+
+  const { user, tokens } = userData;
+
+  const composeData = composeAnAuthResponse(res, tokens, user);
+  const queryData = omitUndefined(composeData);
+
+  const query = new URLSearchParams(queryData);
+
+  return res.redirect(`${ENV.APP_REDIRECT_URL}?` + query.toString());
+}
 
 class UserController {
-  private static async authUser(req: Request, res: Response, next: NextFunction) {
-    try {
-      const userAgent = req.useragent;
-      const connectionData = {
-        ip: req.socket.remoteAddress,
-        browser: userAgent ? userAgent.browser : "",
-        system: userAgent ? userAgent.os : ""
-      } as Omit<TConnectionData, "date">;
-
-      const userData = await UserService.signUp(req.body, connectionData);
-      res.cookie("refreshToken", userData.tokens.refreshToken, refreshTokenCookieOptions);
-      res.cookie("accessToken", userData.tokens.accessToken, accessTokenCookieOptions);
-
-      logger.info(userData);
-      return res.json(userData.user);
-    } catch (error: unknown) {
-      return next(error); // -->error.middleware
-    }
-  }
-
   static async oauthGoogle(req: Request, res: Response, next: NextFunction) {
-    const code = req.query.code;
     try {
-      const { access_token } = await TokenService.getGoogleOauthTokens(code as string);
+      const code = req.query.code;
+      const codeVerifier = decodeURIComponent(req.cookies["codeVerifier"]);
 
-      const googleUser = await TokenService.getGoogleUserInfo(access_token);
+      const { access_token } = await OAuthService.getOauthTokens<IGoogleTokensResult>(
+        "google",
+        code as string,
+        codeVerifier
+      );
+
+      const googleUser = await OAuthService.getUserInfo<IGoogleUserResult>(
+        "google",
+        access_token as string
+      );
 
       if (!googleUser.verified_email) throw ApiError.Forbidden("Google account is not verified.");
 
-      const requestBody: TCreateUser["body"] = {
+      const authUser: TCreateUser = {
         name: googleUser.given_name,
         surname: googleUser.family_name,
         email: googleUser.email,
-        providersID: [`google:${googleUser.id}`],
+        providersId: [`google:${googleUser.id}`],
         role: "user",
         image: googleUser.picture
       };
 
-      req.body = requestBody;
-      await UserController.authUser(req, res, next);
+      await authProcess(req, res, authUser);
     } catch (error: any) {
       return next(error); // -->error.middleware
     }
   }
 
-  static async login(req: Request, res: Response, next: NextFunction) {
+  static async oauthVkontakte(req: Request, res: Response, next: NextFunction) {
     try {
-    } catch (error: unknown) {
+      const { code, device_id } = req.query;
+      const codeVerifier = decodeURIComponent(req.cookies["codeVerifier"]);
+      const { access_token } = await OAuthService.getOauthTokens<IVkontakteTokensResult>(
+        "vkontakte",
+        code as string,
+        codeVerifier,
+        device_id as string
+      );
+
+      const { user: vkontakteUser } = await OAuthService.getUserInfo<IVkontakteUserResult>(
+        "vkontakte",
+        access_token
+      );
+
+      const authUser: TCreateUser = {
+        name: vkontakteUser.first_name,
+        surname: vkontakteUser.last_name,
+        email: vkontakteUser.email,
+        providersId: [`vkontakte:${vkontakteUser.user_id}`],
+        role: "user",
+        image: vkontakteUser.avatar
+      };
+
+      await authProcess(req, res, authUser);
+      return;
+    } catch (error: any) {
       return next(error); // -->error.middleware
     }
   }
 
+  static async oauthYandex(req: Request, res: Response, next: NextFunction) {
+    try {
+      const code = req.query.code;
+      const codeVerifier = decodeURIComponent(req.cookies["codeVerifier"]);
+      const { access_token } = await OAuthService.getOauthTokens<IYandexTokensResult>(
+        "yandex",
+        code as string,
+        codeVerifier
+      );
+
+      const yandexUser = await OAuthService.getUserInfo<IYandexUserResult>("yandex", access_token);
+
+      const authUser: TCreateUser = {
+        name: yandexUser.first_name,
+        surname: yandexUser.last_name,
+        email: yandexUser.default_email,
+        providersId: [`yandex:${yandexUser.id}`],
+        role: "user",
+        image: !yandexUser.is_avatar_empty
+          ? `https://avatars.yandex.net/get-yapic/${yandexUser.default_avatar_id}/islands-200`
+          : undefined
+      };
+
+      await authProcess(req, res, authUser);
+    } catch (error: any) {
+      return next(error); // -->error.middleware
+    }
+  }
+
+  static async signUpWithEmail(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { username, email } = req.body;
+      const { registrationEmail } = req.cookies;
+
+      await EmailService.validateEmail(registrationEmail, email);
+
+      const userData: TCreateUser = {
+        name: "",
+        surname: "",
+        username,
+        email,
+        providersId: [`email:${email}`],
+        role: "user",
+        image: undefined
+      };
+
+      const connectionData = getConnectionData(req);
+      const { user, tokens } = await UserService.signUp(userData, connectionData);
+
+      const composeData = composeAnAuthResponse(res, tokens, user);
+
+      res.clearCookie("registrationEmail", registrationEmailClearCookieOptions);
+
+      return res.status(200).json(composeData);
+    } catch (error: any) {
+      return next(error); // --> error.middleware.ts
+    }
+  }
+
+  static async signInWitEmail(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { email } = req.body;
+      const { registrationEmail } = req.cookies;
+
+      await EmailService.validateEmail(registrationEmail, email);
+
+      const providerId = `email:${email}`;
+
+      const connectionData = getConnectionData(req);
+      const { user, tokens } = await UserService.signIn(providerId, connectionData);
+      if (!user) throw ApiError.Unauthorized();
+
+      const composeData = composeAnAuthResponse(res, tokens, user);
+
+      res.clearCookie("registrationEmail", registrationEmailClearCookieOptions);
+
+      return res.status(200).json(composeData);
+    } catch (error: any) {
+      return next(error); // --> error.middleware.ts
+    }
+  }
+
   static async signOut(req: Request, res: Response, next: NextFunction) {
-    try {
-    } catch (error: unknown) {
-      return next(error); // -->error.middleware.ts
-    }
-  }
+    const clearCookies = () => {
+      res.clearCookie("refreshToken", refreshTokenClearCookieOptions);
+      res.clearCookie("accessToken", accessTokenClearCookieOptions);
+    };
 
-  static async getUser(req: Request, res: Response, next: NextFunction) {
     try {
-    } catch (error: unknown) {
-      return next(error); // -->error.middleware.ts
-    }
-  }
+      const { refreshToken } = req.cookies;
+      await UserService.signOut(refreshToken);
 
-  static async deleteUser(req: Request, res: Response, next: NextFunction) {
-    try {
-    } catch (error: unknown) {
-      return next(error); // -->error.middleware.ts
-    }
-  }
+      clearCookies();
 
-  static async refresh(req: Request, res: Response, next: NextFunction) {
-    try {
-    } catch (error: unknown) {
+      return res.sendStatus(200);
+    } catch (error: any) {
+      clearCookies();
       return next(error); // -->error.middleware.ts
     }
   }
