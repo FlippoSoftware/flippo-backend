eb36859edcb44a2cbad3d88f8275b8645b89a075
BlackPoretsky <84518407+BlackPoretsky@users.noreply.github.com>
2024-10-19 18:44:47 +0300

    Feat: extending the Error class

--- backend/src/exceptions/api.error.ts
+++ backend/src/exceptions/api.error.ts
@@ -1,27 +1,39 @@
 class ApiError extends Error {
   status: number;
-  errors?: [];
+  errors?: any[];
 
-  constructor(status: number, message: string, errors?: []) {
+  constructor(status: number, message: string, errors?: any[]) {
     super(message);
     this.status = status;
     this.errors = errors;
   }
 
-  static UnauthorizedError() {
-    return new ApiError(401, "Пользователь не авторизован");
+  static Unauthorized() {
+    return new ApiError(401, "Unauthorized.");
   }
 
-  static BadRequest(message: string, errors: [] = []) {
+  static BadRequest(message: string, errors: any[] = []) {
     return new ApiError(400, message, errors);
   }
 
-  static ServiceUnavailable(message: string, errors: [] = []) {
+  static ServiceUnavailable(message: string, errors: any[] = []) {
     return new ApiError(503, message, errors);
   }
 
-  static Forbidden(message: string, errors: [] = []) {
+  static Forbidden(message: string, errors: any[] = []) {
     return new ApiError(403, message, errors);
+  }
+
+  static Internal(message: string, errors: any[] = []) {
+    return new ApiError(500, message, errors);
+  }
+
+  static NotFound() {
+    return new ApiError(404, "Data not found.");
+  }
+
+  static Expired(message: string, errors: any[] = []) {
+    return new ApiError(410, message, errors);
   }
 }
 
