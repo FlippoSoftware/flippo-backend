d1a5731b8affcbbe0e91a1c5a3f43797d3f98d42
BlackPoretsky <84518407+BlackPoretsky@users.noreply.github.com>
2024-10-19 20:28:46 +0300

    Feat: OAuthButton component

---
+++ frontend/src/modules/Auth/ui/OAuthButton/story/OAuthButton.stories.ts
@@ -0,0 +1,25 @@
+import OAuthButton from "../ui/OAuthButton";
+
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta = {
+  title: "Auth/ui/OAuthButton",
+  component: OAuthButton,
+  tags: ["autodocs"]
+};
+
+export default meta;
+
+type OAuthButtonStory = StoryObj<typeof OAuthButton>;
+
+export const OAuthButtonGoogle: OAuthButtonStory = {
+  args: {
+    provider: "google"
+  }
+};
+
+export const OAuthButtonVK: OAuthButtonStory = {
+  args: {
+    provider: "vkontakte"
+  }
+};
--- frontend/src/shared/ui/Button/OAuthButton/types/IOAuthButtonProps.ts
+++
@@ -1,7 +0,0 @@
-import { ButtonHTMLAttributes } from "react";
-
-interface IOAuthButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  provider: "Google" | "Vkontakte";
-}
-
-export { type IOAuthButtonProps };
