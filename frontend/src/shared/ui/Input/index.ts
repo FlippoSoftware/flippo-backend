8426d99c5f93d98c1a6e086c276fa27325985497
BlackPoretsky <84518407+BlackPoretsky@users.noreply.github.com>
2024-10-19 20:20:10 +0300

    Feat: InputVerificationCode component

---
+++ frontend/src/shared/ui/Input/index.ts
@@ -0,0 +1,3 @@
+export { default as UnstyledInput } from "./ui/Input";
+export { default as FormInput } from "./FormInput/ui/FormInput";
+export { default as InputVerificationCode } from "./InputVerificationCode/ui/InputVerificationCode";
