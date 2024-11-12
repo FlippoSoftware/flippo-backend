4b18a0343f9b1eae5f5d90b35c344efbdf398c93
BlackPoretsky <84518407+BlackPoretsky@users.noreply.github.com>
2024-11-12 17:29:11 +0300

    Update: processing an empty dictionary

--- frontend/src/shared/utils/messageByKey.utils.ts
+++ frontend/src/shared/utils/messageByKey.utils.ts
@@ -1,4 +1,3 @@
-import { createEffect } from "effector";
 import { createTranslator } from "next-intl";
 import { type TInternationalizationLocales } from "@shared/constants/internationalization.constant";
 
@@ -7,25 +6,30 @@ import { getLocale } from "./getLocale.utils";
 type TMessages = { locale: TInternationalizationLocales; dictionary: IntlMessages };
 let messages: TMessages | undefined = undefined;
 
-export const errorMessageByKeyFx = createEffect<{ key: string; namespace?: string }, string>(
-  async ({ key, namespace }) => {
-    const locale = getLocale();
+export async function errorMessageByKey(key: string, namespace?: string) {
+  const locale = getLocale();
 
-    if (typeof messages === "undefined" || messages.locale !== locale) {
-      messages = {
-        locale,
-        dictionary: await import(`../../../messages/${locale}.json`)
-      } as TMessages;
-    }
-
-    const t = createTranslator({ locale, messages: messages.dictionary });
-
-    if (namespace && t.has(`${namespace}.${key}` as any)) {
-      return t(`${namespace}.${key}` as any);
-    }
-
-    if (t.has(`error.${key}` as any)) return t(`error.${key}` as any);
-
-    return t("error.500");
+  // Attempt to upload a localization file
+  if (typeof messages === "undefined" || messages.locale !== locale) {
+    messages = {
+      locale,
+      dictionary: await import(`../../../messages/${locale}.json`).catch(() => undefined)
+    } as TMessages;
   }
-);
+
+  // Checking if the dictionary was loaded successfully
+  if (!messages.dictionary) {
+    console.error(`Failed to load messages for locale ${locale}`);
+    return "ERROR: An error occurred while retrieving the error message. Sorry for the problems.";
+  }
+
+  const t = createTranslator({ locale, messages: messages.dictionary });
+
+  if (namespace && t.has(`${namespace}.${key}` as any)) {
+    return t(`${namespace}.${key}` as any);
+  }
+
+  if (t.has(`error.${key}` as any)) return t(`error.${key}` as any);
+
+  return t("error.500");
+}
