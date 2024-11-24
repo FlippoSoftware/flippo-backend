054e7d6dd79de1301646fb583a935cf6e0d34706
BlackPoretsky <84518407+BlackPoretsky@users.noreply.github.com>
2024-11-25 02:10:55 +0300

    Move, refactor: moved UnstyledButton from old project config

---
+++ _frontend/src/shared/ui/Button/types/TUnstyledButtonProps.ts
@@ -0,0 +1,10 @@
+import type { ComponentPropsWithRef, ElementType } from 'react';
+
+type TUnstyledButtonOwnProps<E extends ElementType> = {
+  as: E;
+};
+
+type TUnstyledButtonProps<E extends ElementType> = Omit<ComponentPropsWithRef<E>, keyof TUnstyledButtonOwnProps<E>> &
+  TUnstyledButtonOwnProps<E>;
+
+export { type TUnstyledButtonProps };
