dfb58f95f36c74de6e2376961fbb86f6049b271f
BlackPoretsky <84518407+BlackPoretsky@users.noreply.github.com>
2024-11-25 02:25:23 +0300

    Feat: Link component

---
+++ _frontend/src/shared/ui/Link/ui/Link.tsx
@@ -0,0 +1,21 @@
+import { ArrowIcon } from '@icons/ArrowIcon';
+import clsx from 'clsx';
+
+import { type TLinkProps } from '../type/TLinkProps';
+import st from './Link.module.scss';
+
+function Link(props: TLinkProps) {
+  const { children, icon, variant = 'neutral', ...otherProps } = props;
+
+  return (
+    <a {...otherProps} className={clsx(st.anchor, st[variant])}>
+      {icon ? <div className={st.icon}>{icon}</div> : null}
+      <div className={st.text}>
+        <span>{children}</span>
+        <ArrowIcon className={st.arrow} type={'link'} />
+      </div>
+    </a>
+  );
+}
+
+export default Link;
