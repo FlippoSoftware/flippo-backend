6ea16fc067cbe621e0284038a2ff5aa824b55913
BlackPoretsky <84518407+BlackPoretsky@users.noreply.github.com>
2024-11-28 11:59:35 +0300

    Update: seted button tag as default

--- _frontend/src/shared/ui/Button/types/TUnstyledButtonProps.ts
+++ _frontend/src/shared/ui/Button/types/TUnstyledButtonProps.ts
@@ -1,7 +1,7 @@
 import type { ComponentPropsWithRef, ElementType } from 'react';
 
 type TUnstyledButtonOwnProps<E extends ElementType> = {
-  as: E;
+  as?: E;
 };
 
 type TUnstyledButtonProps<E extends ElementType> = Omit<ComponentPropsWithRef<E>, keyof TUnstyledButtonOwnProps<E>> &
