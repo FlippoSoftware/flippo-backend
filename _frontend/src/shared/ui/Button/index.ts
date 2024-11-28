c4b50755d0f958d9f41789d2b75cf98db219ff0e
BlackPoretsky <84518407+BlackPoretsky@users.noreply.github.com>
2024-11-28 22:26:31 +0300

    Update: added reexport TUnstyledButtonProps

--- _frontend/src/shared/ui/Button/index.ts
+++ _frontend/src/shared/ui/Button/index.ts
@@ -1,4 +1,6 @@
 export { default as Button } from './Button/ui/Button';
 export { default as IconButton } from './IconButton/ui/IconButton';
 export { default as LoadingButton } from './LoadingButton/ui/LoadingButton';
+
+export type { TUnstyledButtonProps } from './types/TUnstyledButtonProps';
 export { default as UnstyledButton } from './ui/UnstyledButton';
