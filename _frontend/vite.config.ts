6eff20c9c6a4150cd5bbca109310c237fc410ad4
BlackPoretsky <84518407+BlackPoretsky@users.noreply.github.com>
2024-11-28 22:08:50 +0300

    Update: added alias fro paths

--- _frontend/vite.config.ts
+++ _frontend/vite.config.ts
@@ -23,13 +23,21 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': '/src',
+      '@api': '/src/shared/api/*',
+      '@app': '/src/app',
       '@env': '/src/shared/env',
+      '@exceptions': '/src/shared/exceptions/*',
       '@hooks': '/src/shared/hooks',
-      '@i18next': '/src/i18next',
       '@icons': '/src/shared/icons',
+      '@models': '/src/shared/models/*',
+      '@modules': '/src/modules',
+      '@pages': '/src/pages',
+      '@query': '/src/shared/query/*',
       '@schemas': '/src/shared/schemas',
       '@settings': '/src/settings',
+      '@shared': '/src/shared',
       '@styles': '/src/settings/styles',
+      '@surreal': '/src/shared/surreal/*',
       '@ui': '/src/shared/ui',
       '@utils': '/src/shared/utils',
       '@widgets': '/src/widgets'
