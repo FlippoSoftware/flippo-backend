d9363b6ce3435770d0c8f6121e95831632a9403e
BlackPoretsky <84518407+BlackPoretsky@users.noreply.github.com>
2024-12-03 22:56:21 +0300

    Update: off rule '@typescript-eslint/no-unsafe-member-access'

--- _frontend/eslint.config.js
+++ _frontend/eslint.config.js
@@ -75,6 +75,7 @@ export default [
       '@typescript-eslint/no-namespace': 'off',
       '@typescript-eslint/no-unsafe-argument': 'off',
       '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
       '@typescript-eslint/no-unused-expressions': [
         'error',
         {
