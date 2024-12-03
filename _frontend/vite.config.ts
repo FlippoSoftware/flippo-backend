11e332fd451b9bd934791bc0f84a8c3f2c7d6433
BlackPoretsky <84518407+BlackPoretsky@users.noreply.github.com>
2024-12-03 22:51:13 +0300

    Update: updated alias

--- _frontend/vite.config.ts
+++ _frontend/vite.config.ts
@@ -22,24 +22,11 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': '/src',
-      '@api': '/src/shared/api/*',
       '@app': '/src/app',
-      '@env': '/src/shared/env',
-      '@exceptions': '/src/shared/exceptions/*',
-      '@hooks': '/src/shared/hooks',
-      '@icons': '/src/shared/icons',
-      '@models': '/src/shared/models/*',
       '@modules': '/src/modules',
       '@pages': '/src/pages',
-      '@query': '/src/shared/query/*',
-      '@schemas': '/src/shared/schemas',
       '@settings': '/src/settings',
       '@shared': '/src/shared',
-      '@styles': '/src/settings/styles',
-      '@surreal': '/src/shared/surreal/*',
-      '@ui': '/src/shared/ui',
-      '@utils': '/src/shared/utils',
       '@widgets': '/src/widgets'
     }
   },
