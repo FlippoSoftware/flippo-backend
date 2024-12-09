15c334f28b62c6aad37d2e916a6c412d1e6e7863
BlackPoretsky <84518407+BlackPoretsky@users.noreply.github.com>
2024-12-09 14:21:34 +0300

    Update: the type of prop is extended from HTMLHRElement

--- frontend/src/shared/ui/Separator/ui/Separator.tsx
+++ frontend/src/shared/ui/Separator/ui/Separator.tsx
@@ -4,11 +4,11 @@ import { type TSeparatorProps } from '../types/TSeparatorProps';
 import st from './Separator.module.scss';
 
 function Separator(props: TSeparatorProps) {
-  const { orientation = 'horizontal', spacing } = props;
+  const { orientation = 'horizontal', spacing, ...otherProps } = props;
 
   return (
-    <div className={clsx(st.separatorWrap, st[orientation], !!spacing && st[spacing])}>
-      <hr className={st.separator} />
+    <div className={clsx(st.separatorWrap, st[orientation], !!spacing && st[spacing])} role={'presentation'}>
+      <hr className={st.separator} {...otherProps} />
     </div>
   );
 }
