d76563f5a597c8434288bb76387cd904b5eee3b0
BlackPoretsky <84518407+BlackPoretsky@users.noreply.github.com>
2024-12-13 20:43:17 +0300

    Delete: the obsolete implementation has been removed

--- frontend/src/shared/ui/MenuItem/index.tsx
+++
@@ -1,5 +0,0 @@
-import MenuItem from './ui/MenuItem';
-
-export type * from './types/TMenuItemProps';
-export { default as MenuItem } from './ui/MenuItem';
-export default MenuItem;
--- frontend/src/shared/ui/OptionItem/index.ts
+++
@@ -1,2 +0,0 @@
-export type * from './types/TOptionItemProps';
-export { default as OptionItem } from './ui/OptionItem';
