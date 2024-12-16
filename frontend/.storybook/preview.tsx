3cfa3ffe1918582469bca33cc577bce8ec810698
BlackPoretsky <84518407+BlackPoretsky@users.noreply.github.com>
2024-12-17 00:59:23 +0300

    Update: replaced i18n on i18n from @withease/i18n

--- frontend/.storybook/preview.tsx
+++ frontend/.storybook/preview.tsx
@@ -2,19 +2,25 @@ import type { Preview } from '@storybook/react';
 
 import '../src/settings/styles/global.scss';
 
+import { router } from '@settings/routing';
 import { ToastContainer } from '@widgets/ToastNotification';
+import { RouterProvider } from 'atomic-router-react';
 import { Suspense } from 'react';
 
-import i18n from '../src/settings/i18next/i18next.config';
+import { $i18n, initI18next } from '../src/settings/i18next/i18next.config';
 
-await i18n.init();
+initI18next();
+// eslint-disable-next-line effector/no-getState
+const i18n = $i18n.getState();
 
 const preview: Preview = {
   decorators: (Story) => (
-    <Suspense fallback={'Loading...'}>
-      <Story />
-      <ToastContainer toastCountOnScreen={4} />
-    </Suspense>
+    <RouterProvider router={router}>
+      <Suspense fallback={'Loading...'}>
+        <Story />
+        <ToastContainer toastCountOnScreen={4} />
+      </Suspense>
+    </RouterProvider>
   ),
   initialGlobals: {
     locale: 'ru',
