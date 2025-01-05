9027c31e1978e16ee2bb52c2b9b95f74f81e9455
BlackPoretsky <84518407+BlackPoretsky@users.noreply.github.com>
2025-01-05 17:50:28 +0300

    Update: enhance app initialization with database authentication and session management

--- frontend/src/app/init/models/app.model.ts
+++ frontend/src/app/init/models/app.model.ts
@@ -1,7 +1,20 @@
 import { $i18n, $isReady, initI18next } from '@settings/i18next';
 import { initRouter } from '@settings/routing';
-import { createEffect, createEvent, sample } from 'effector';
-import { and, not } from 'patronum';
+import { sessionAuth } from '@settings/session';
+import {
+  $db,
+  dbFailConnected,
+  initDb,
+  dbAuthenticateFx as originDbAuthenticateFx,
+  dbDisconnectFx as originDbDisconnectFx,
+  type SurrealError
+} from '@settings/surreal';
+import { sessionAuthFx } from '@shared/api';
+import { attach, createEffect, createEvent, merge, sample, split } from 'effector';
+import { and, delay, not } from 'patronum';
+
+const dbAuthenticateFx = attach({ effect: originDbAuthenticateFx });
+const dbDisconnectFx = attach({ effect: originDbDisconnectFx });
 
 // #region init
 export const initApp = createEvent();
@@ -11,9 +24,40 @@ const $i18nNotNull = $i18n.map((instance) => !!instance);
 const $initAppProcess = initAppFx.pending;
 export const $initialized = and(not($initAppProcess), $isReady, $i18nNotNull);
 
+const dbAuth = createEvent();
+const dbReconnect = createEvent();
+const $dbAuthenticateProcess = dbAuthenticateFx.pending;
+
 sample({
   clock: initApp,
-  target: [initI18next, initAppFx, initRouter]
+  target: [initI18next, initAppFx, initRouter, initDb, sessionAuth]
+});
+
+sample({
+  clock: sessionAuthFx.done,
+  target: dbAuth
+});
+
+sample({
+  clock: dbAuth,
+  source: $db,
+  filter: not($dbAuthenticateProcess),
+  target: dbAuthenticateFx
+});
+
+split({
+  source: dbAuthenticateFx.failData,
+  match: (error: SurrealError) => error.code,
+  cases: {
+    ERR_OFFLINE: dbReconnect,
+    ERR_TOKEN_MISSING: sessionAuthFx
+  }
+});
+
+delay({
+  source: merge([dbFailConnected, dbReconnect]),
+  target: initDb,
+  timeout: 5000
 });
 // #endregion
 
@@ -23,6 +67,6 @@ const teardownI18nextFx = createEffect(() => {});
 
 sample({
   clock: teardownApp,
-  target: teardownI18nextFx
+  target: [teardownI18nextFx, dbDisconnectFx]
 });
 // #endregion
