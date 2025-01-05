c863f38e4883e5a607f18ed233e957ebfbc6b03a
BlackPoretsky <84518407+BlackPoretsky@users.noreply.github.com>
2025-01-05 16:21:04 +0300

    Update: outdated code has been commented out, and in the future it will be rewritten to queries for Surreal in the surql format.

--- frontend/src/shared/api/sets.api.ts
+++ frontend/src/shared/api/sets.api.ts
@@ -1,66 +1,66 @@
-import { record, SetSchema, type TSet } from '@shared/schemas';
-import { getDb } from '@shared/surreal';
-import { z } from 'zod';
+// import { record, SetSchema, type TSet } from '@shared/schemas';
+// import { getDb } from '@settings/surreal';
+// import { z } from 'zod';
 
-async function fetchSets() {
-  const db = await getDb();
-  const sets = await db.select<TSet>('set');
-  return z.array(SetSchema).parse(sets);
-}
+// async function fetchSets() {
+//   const db = await getDb();
+//   const sets = await db.select<TSet>('set');
+//   return z.array(SetSchema).parse(sets);
+// }
 
-async function fetchSet(id: TSet['id']) {
-  id = record('set').parse(id);
+// async function fetchSet(id: TSet['id']) {
+//   id = record('set').parse(id);
 
-  const db = await getDb();
-  const set = await db.select<TSet>(id);
-  return await SetSchema.parseAsync(set).catch(() => undefined);
-}
+//   const db = await getDb();
+//   const set = await db.select<TSet>(id);
+//   return await SetSchema.parseAsync(set).catch(() => undefined);
+// }
 
-async function createSet({
-  cards,
-  description,
-  name,
-  publication
-}: Partial<Pick<TSet, 'cards' | 'description' | 'publication'>> & Pick<TSet, 'name'>) {
-  name = z.string().parse(name);
-  description = z.string().optional().parse(description);
-  cards = z.array(record('card')).optional().parse(cards);
-  publication = record('publication').optional().parse(publication);
+// async function createSet({
+//   cards,
+//   description,
+//   name,
+//   publication
+// }: Partial<Pick<TSet, 'cards' | 'description' | 'publication'>> & Pick<TSet, 'name'>) {
+//   name = z.string().parse(name);
+//   description = z.string().optional().parse(description);
+//   cards = z.array(record('card')).optional().parse(cards);
+//   publication = record('publication').optional().parse(publication);
 
-  const db = await getDb();
-  const [result] = await db.create<
-    TSet,
-    Partial<Pick<TSet, 'cards' | 'description' | 'publication'>> & Pick<TSet, 'name'>
-  >('set', { cards, description, name, publication });
-  return await SetSchema.parseAsync(result).catch(() => undefined);
-}
+//   const db = await getDb();
+//   const [result] = await db.create<
+//     TSet,
+//     Partial<Pick<TSet, 'cards' | 'description' | 'publication'>> & Pick<TSet, 'name'>
+//   >('set', { cards, description, name, publication });
+//   return await SetSchema.parseAsync(result).catch(() => undefined);
+// }
 
-async function updateSet(
-  id: TSet['id'],
-  { cards, description, name, publication }: Partial<Pick<TSet, 'cards' | 'description' | 'name' | 'publication'>>
-) {
-  id = record('set').parse(id);
-  name = z.string().optional().parse(name);
-  description = z.string().optional().parse(description);
-  cards = z.array(record('card')).optional().parse(cards);
-  publication = record('publication').optional().parse(publication);
+// async function updateSet(
+//   id: TSet['id'],
+//   { cards, description, name, publication }: Partial<Pick<TSet, 'cards' | 'description' | 'name' | 'publication'>>
+// ) {
+//   id = record('set').parse(id);
+//   name = z.string().optional().parse(name);
+//   description = z.string().optional().parse(description);
+//   cards = z.array(record('card')).optional().parse(cards);
+//   publication = record('publication').optional().parse(publication);
 
-  const db = await getDb();
-  const [result] = await db.merge<TSet, Partial<Pick<TSet, 'cards' | 'description' | 'name' | 'publication'>>>(id, {
-    cards,
-    description,
-    name,
-    publication
-  });
-  return await SetSchema.parseAsync(result).catch(() => undefined);
-}
+//   const db = await getDb();
+//   const [result] = await db.merge<TSet, Partial<Pick<TSet, 'cards' | 'description' | 'name' | 'publication'>>>(id, {
+//     cards,
+//     description,
+//     name,
+//     publication
+//   });
+//   return await SetSchema.parseAsync(result).catch(() => undefined);
+// }
 
-async function deleteSet(id: TSet['id']) {
-  id = record('set').parse(id);
+// async function deleteSet(id: TSet['id']) {
+//   id = record('set').parse(id);
 
-  const db = await getDb();
-  const [result] = await db.delete<TSet>(id);
-  return await SetSchema.parseAsync(result).catch(() => undefined);
-}
+//   const db = await getDb();
+//   const [result] = await db.delete<TSet>(id);
+//   return await SetSchema.parseAsync(result).catch(() => undefined);
+// }
 
-export { createSet, deleteSet, fetchSet, fetchSets, updateSet };
+// export { createSet, deleteSet, fetchSet, fetchSets, updateSet };
--- frontend/src/shared/api/tags.api.ts
+++ frontend/src/shared/api/tags.api.ts
@@ -1,47 +1,47 @@
-import { record, TagSchema, type TTag } from '@shared/schemas';
-import { getDb } from '@shared/surreal';
-import { z } from 'zod';
+// import { record, TagSchema, type TTag } from '@shared/schemas';
+// import { getDb } from '@settings/surreal';
+// import { z } from 'zod';
 
-async function fetchTags() {
-  const db = await getDb();
-  const tags = await db.select<TTag>('tag');
-  return z.array(TagSchema).parse(tags);
-}
+// async function fetchTags() {
+//   const db = await getDb();
+//   const tags = await db.select<TTag>('tag');
+//   return z.array(TagSchema).parse(tags);
+// }
 
-async function fetchTag(id: TTag['id']) {
-  id = record('tag').parse(id);
+// async function fetchTag(id: TTag['id']) {
+//   id = record('tag').parse(id);
 
-  const db = await getDb();
-  const tag = await db.select<TTag>(id);
-  return await TagSchema.parseAsync(tag).catch(() => undefined);
-}
+//   const db = await getDb();
+//   const tag = await db.select<TTag>(id);
+//   return await TagSchema.parseAsync(tag).catch(() => undefined);
+// }
 
-async function createTag({ name }: Pick<TTag, 'name'>) {
-  name = z.string().parse(name);
+// async function createTag({ name }: Pick<TTag, 'name'>) {
+//   name = z.string().parse(name);
 
-  const db = await getDb();
-  const [result] = await db.create<TTag, Pick<TTag, 'name'>>('tag', { name });
-  return await TagSchema.parseAsync(result).catch(() => undefined);
-}
+//   const db = await getDb();
+//   const [result] = await db.create<TTag, Pick<TTag, 'name'>>('tag', { name });
+//   return await TagSchema.parseAsync(result).catch(() => undefined);
+// }
 
-async function updateTag(id: TTag['id'], { name }: Partial<Pick<TTag, 'name'>>) {
-  id = record('tag').parse(id);
-  name = z.string().optional().parse(name);
+// async function updateTag(id: TTag['id'], { name }: Partial<Pick<TTag, 'name'>>) {
+//   id = record('tag').parse(id);
+//   name = z.string().optional().parse(name);
 
-  const db = await getDb();
-  const [result] = await db.merge<TTag, Partial<Pick<TTag, 'name'>>>(id, {
-    name
-  });
+//   const db = await getDb();
+//   const [result] = await db.merge<TTag, Partial<Pick<TTag, 'name'>>>(id, {
+//     name
+//   });
 
-  return await TagSchema.parseAsync(result).catch(() => undefined);
-}
+//   return await TagSchema.parseAsync(result).catch(() => undefined);
+// }
 
-async function deleteTag(id: TTag['id']) {
-  id = record('tag').parse(id);
+// async function deleteTag(id: TTag['id']) {
+//   id = record('tag').parse(id);
 
-  const db = await getDb();
-  const [result] = await db.delete<TTag>(id);
-  return await TagSchema.parseAsync(result).catch(() => undefined);
-}
+//   const db = await getDb();
+//   const [result] = await db.delete<TTag>(id);
+//   return await TagSchema.parseAsync(result).catch(() => undefined);
+// }
 
-export { createTag, deleteTag, fetchTag, fetchTags, updateTag };
+// export { createTag, deleteTag, fetchTag, fetchTags, updateTag };
